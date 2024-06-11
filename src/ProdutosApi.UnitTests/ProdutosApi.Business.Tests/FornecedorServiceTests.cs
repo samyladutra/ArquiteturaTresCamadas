3fc15073b810a7697a7af2299b776ad49fbdedd8
Samyla Dutra <samyladutra@gmail.com>
2024-06-10 21:52:02 -0300

    feat: inclusão projeto de testes unitários

---
+++ src/ProdutosApi.UnitTests/ProdutosApi.Business.Tests/FornecedorServiceTests.cs
@@ -0,0 +1,13 @@
+using Moq;
+using ProdutosApi.Business.Models;
+
+namespace ProdutosApi.Business.Tests;
+
+public class FornecedorServiceTests
+{
+    [Fact]
+    public void Atualizar_Fornecedor_Deve_Retornar_Sucesso()
+    {
+        var fornecedor = new Mock<Fornecedor>();
+    }
+}
