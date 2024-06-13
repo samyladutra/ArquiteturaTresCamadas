a34a15578f8c3d42d1913ddc54abb0088a690470
Samyla Dutra <samyladutra@gmail.com>
2024-06-12 22:19:34 -0300

    feat: desenvolvimento teste unitario para FornecedorService

--- src/ProdutosApi.UnitTests/ProdutosApi.Business.Tests/FornecedorServiceTests.cs
+++ src/ProdutosApi.UnitTests/ProdutosApi.Business.Tests/FornecedorServiceTests.cs
@@ -1,18 +1,36 @@
+using FluentAssertions;
 using Moq;
+using ProdutosApi.Business.Interfaces;
 using ProdutosApi.Business.Models;
+using ProdutosApi.Business.Notificacoes;
+using ProdutosApi.Business.Services;
 
-namespace ProdutosApi.Business.Tests;
-
-public class FornecedorServiceTests
+namespace ProdutosApi.Business.Tests
 {
-    [Fact]
-    public void Atualizar_Fornecedor_Deve_Retornar_Sucesso()
+    public class FornecedorServiceTests
     {
-        var fornecedor = new Mock<Fornecedor>();
-        var fornecedorService = new Mock<FornecedorService>();
+        [Fact]
+        public async void Atualizar_Fornecedor_Deve_Retornar_Sucesso()
+        {
+            //arrange
+            var fornecedor = new FornecedorBuilder()
+                                .ComNome("")
+                                .ComDocumento("97310212053")
+                                .Ativo(true)
+                                .Build();
 
-        fornecedorService.Adicionar(fornecedor);
+            var notificador = new Mock<INotificador>();
+            var fornecedorService = new FornecedorService(new Mock<IFornecedorRepository>().Object, notificador.Object);
 
-        
+            var notificacao = new Notificacao("O campo Nome precisa ser fornecido");
+            notificador.Setup(n => n.ObterNotificacoes()).Returns(new List<Notificacao>() { notificacao });
+
+            //act
+            await fornecedorService.Adicionar(fornecedor);
+
+            //assert
+            var teste = notificador.Object.ObterNotificacoes();
+            notificador.Object.ObterNotificacoes().Contains(notificacao);
+        }
     }
 }
