a8f07714c9c852ce3452d062d3fb8f274b66d469
Samyla Dutra <samyladutra@gmail.com>
2024-06-18 21:08:28 -0300

    wip: correções para criação de migrations

--- src/ProdutosApi.Data/Context/MeuDbContext.cs
+++ src/ProdutosApi.Data/Context/MeuDbContext.cs
@@ -3,45 +3,46 @@ using ProdutosApi.Business.Models;
 
 namespace ProdutosApi.Data.Context;
 public class MeuDbContext : DbContext
-    {        public MeuDbContext(DbContextOptions<MeuDbContext> options) : base(options)
+{
+    public MeuDbContext(DbContextOptions<MeuDbContext> options) : base(options)
+    {
+        ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        ChangeTracker.AutoDetectChangesEnabled = false;
+    }
+
+    public ISet<Produto> Produtos { get; set; }
+    public DbSet<Endereco> Enderecos { get; set; }
+    public DbSet<Fornecedor> Fornecedores { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // foreach (var property in modelBuilder.Model.GetEntityTypes()
+        //     .SelectMany(e => e.GetProperties()
+        //         .Where(p => p.ClrType == typeof(string))))
+        //     property.SetColumnType("varchar(100)");
+
+        // modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+
+        // foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
         {
-            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
-            ChangeTracker.AutoDetectChangesEnabled = false;
-        }
-
-        public ISet<Produto> Produtos { get; set; }
-        public DbSet<Endereco> Enderecos { get; set; }
-        public DbSet<Fornecedor> Fornecedores { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            // foreach (var property in modelBuilder.Model.GetEntityTypes()
-            //     .SelectMany(e => e.GetProperties()
-            //         .Where(p => p.ClrType == typeof(string))))
-            //     property.SetColumnType("varchar(100)");
-
-            // modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
-
-            // foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
-
-            base.OnModelCreating(modelBuilder);
-        }
-
-        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
-        {
-            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            if (entry.State == EntityState.Added)
             {
-                if (entry.State == EntityState.Added)
-                {
-                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
-                }
-
-                if (entry.State == EntityState.Modified)
-                {
-                    entry.Property("DataCadastro").IsModified = false;
-                }
+                entry.Property("DataCadastro").CurrentValue = DateTime.Now;
             }
 
-            return base.SaveChangesAsync(cancellationToken);
+            if (entry.State == EntityState.Modified)
+            {
+                entry.Property("DataCadastro").IsModified = false;
+            }
         }
+
+        return base.SaveChangesAsync(cancellationToken);
     }
+}
