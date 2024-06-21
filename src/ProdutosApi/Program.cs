c95e4770d3265633abdc0479604ad84a7f0c29f3
Samyla Dutra <samyladutra@gmail.com>
2024-06-21 18:00:50 -0300

    fix: referenciando resolucao de dependencias

--- src/ProdutosApi/Program.cs
+++ src/ProdutosApi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ProdutosApi.Data.Context;
+using ProdutosApi.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,8 @@ builder.Services.AddDbContext<MeuDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.ResolveDependencies();
 
 var app = builder.Build();
 
