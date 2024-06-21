0baa689a4656d5661192bda5e79d7eaa7ce8378f
Samyla Dutra <44510520+samyladutra@users.noreply.github.com>
2024-06-20 21:14:45 -0300

    Feature/dev camada acesso dados (#3)
    * feat: Inclusão repository
    
    * feat: especializando repositorios das entidades
    
    * feat: inclusão de viewmodels

---
+++ src/ProdutosApi.Data/Repository/ProdutoRepository.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using ProdutosApi.Business.Interfaces;
+using ProdutosApi.Business.Models;
+using ProdutosApi.Data.Context;
+
+namespace ProdutosApi.Data.Repository;
+
+public class ProdutoRepository : Repository<Produto>, IProdutoRepository
+    {
+        public ProdutoRepository(MeuDbContext context) : base(context) { }
+
+        public async Task<Produto> ObterProdutoFornecedor(Guid id)
+        {
+            return await Db.Produtos.AsNoTracking()
+                .Include(f => f.Fornecedor)
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<IEnumerable<Produto>> ObterProdutosFornecedores()
+        {
+            return await Db.Produtos.AsNoTracking()
+                .Include(f => f.Fornecedor)
+                .OrderBy(p => p.Nome)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Produto>> ObterProdutosPorFornecedor(Guid fornecedorId)
+        {
+            return await Buscar(p => p.FornecedorId == fornecedorId);
+        }
+    }
