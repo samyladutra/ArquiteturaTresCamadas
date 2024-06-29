1bb850ab6252e0f05974ca0775baf85171644890
Samyla Dutra <44510520+samyladutra@users.noreply.github.com>
2024-06-28 23:20:29 -0300

    Main: inclusão camada de aplicacao (#6)
    * feat: criacao repositorio
    
    * feat: criando os modelos, services e tratamento de notificações
    
    * refactor: corrigindo identação
    
    * feat: inclusão projeto de testes unitários
    
    * wip: Inclusão projeto de testes unitarios
    
    * feat: desenvolvimento teste unitario para FornecedorService
    
    * feat: Inclusão EntityFrameworkCore
    
    * feat: mapeando entidades para base de dados
    
    * test: inclusao teste para endereco invalido
    
    * feat: inclusão projeto webapi e configuração das migrations
    
    * Feature/dev camada acesso dados (#3)
    
    * feat: Inclusão repository
    
    * feat: especializando repositorios das entidades
    
    * feat: inclusão de viewmodels
    
    * test: implementando cenários de teste
    
    * Develop: Feature camada aplicacao (#5)
    
    * wip: iniciando implementação da controller
    
    * feat: inclusão automapper
    
    * fix: referenciando resolucao de dependencias
    
    * feat: desenvolvendo as funcionalidades de negócio
    
    * feat: implementando mensagens de erro e notificações
    
    * test: inclusão cenários de teste
    
    * test: Inclusão cenários de testes FornecedorService
    
    * refactor: incluindo operador virtual no repository para testes

--- src/ProdutosApi.UnitTests/ProdutosApi.Business.Tests/Builder/FornecedorBuilder.cs
+++ src/ProdutosApi.UnitTests/ProdutosApi.Business.Tests/Builder/FornecedorBuilder.cs
@@ -8,6 +8,12 @@ public class FornecedorBuilder
         _fornecedor = new Fornecedor();
     }
 
+    public FornecedorBuilder ComId(Guid id)
+    {
+        _fornecedor.Id = id;
+        return this;
+    }
+
     public FornecedorBuilder ComNome(string nome)
     {
         _fornecedor.Nome = nome;
@@ -20,7 +26,7 @@ public class FornecedorBuilder
         return this;
     }
 
-    public FornecedorBuilder ComTipoFornecedor(TipoFornecedor tipoFornecedor)
+    public FornecedorBuilder DoTipo(TipoFornecedor tipoFornecedor)
     {
         _fornecedor.TipoFornecedor = tipoFornecedor;
         return this;
@@ -32,15 +38,17 @@ public class FornecedorBuilder
         return this;
     }
 
+    public FornecedorBuilder ComProdutos(List<Produto> produtos)
+    {
+        _fornecedor.Produtos = produtos;
+        return this;
+    }
+
     public FornecedorBuilder Ativo(bool ativo)
     {
         _fornecedor.Ativo = ativo;
         return this;
     }
-
-
-
-
 
     public Fornecedor Build()
     {
--- src/ProdutosApi/Program.cs
+++ src/ProdutosApi/Program.cs
@@ -1,7 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using ProdutosApi.Data.Context;
+using ProdutosApi.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -12,6 +16,10 @@ builder.Services.AddDbContext<MeuDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.ResolveDependencies();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -21,5 +29,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
